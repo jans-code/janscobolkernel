533c5af9f6725cd437df64ce2833ceed9916b28f
jans-code <7jantb@gmx.de>
2023-04-22 14:17:27 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# janscobolkernel
+
+![Logo](janscobolkernel/logo-svg.svg)
+
+A jupyter kernel implementation of [GnuCOBOL](https://gnucobol.sourceforge.io).
+
+## Dev Installation
+
+- install gnucobol from your package repository
+- download/clone this project
+- open shell in project folder
+- `pip install -e ./`
+- then install kernelspec
+- `janscobolkernel`
+- or
+- `jupyter kernelspec install --user janscobolkernel`
+
+## Uninstall
+
+- `jupyter kernelspec uninstall janscobolkernel`
+- `pip uninstall janscobolkernel`
---
+++ src/janscobolkernel/kernel.py
@@ -0,0 +1,41 @@
+##!/usr/bin/env python
+from ipykernel.kernelbase import Kernel
+import pexpect, os, shutil
+
+workingdir = "/tmp/cobolkernel/"
+
+class janscobolkernel(Kernel):
+    implementation = 'IPython'
+    implementation_version = '8.12.0'
+    language = 'cobol'
+    language_version = '3.1.2'
+    language_info = {
+        'name': 'cobol',
+        'mimetype': 'application/cobol',
+        'file_extension': '.cob',
+    }
+    banner = "COBOL kernel"
+
+    def do_execute(self, code, silent, store_history=True, user_expressions=None,
+                   allow_stdin=False):
+        if not silent:
+            if os.path.exists(workingdir):
+                shutil.rmtree(workingdir)
+            os.mkdir(workingdir)
+            os.chdir(workingdir)
+            with open(workingdir + "proj.cob", "w") as f:
+                    f.write(code)
+            solution = pexpect.run('cobc -Ox ' + workingdir  + 'proj.cob').decode('UTF-8')
+            if os.path.exists(workingdir + 'proj'):
+                solution = pexpect.run(workingdir + 'proj').decode('UTF-8')
+            stream_content = {'name': 'stdout', 'text': solution}
+            self.send_response(self.iopub_socket, 'stream', stream_content)
+
+        return {'status': 'ok',
+                'execution_count': self.execution_count,
+                'payload': [],
+                'user_expressions': {},
+               }
+
+    def do_shutdown(self, restart):
+        shutil.rmtree(workingdir)
